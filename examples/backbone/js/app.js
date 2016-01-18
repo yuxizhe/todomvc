4f39ed3000c192ee659102f21e793558e58957d4
yuxizhe <yuxizhe2008@163.com>
2016-01-19 00:28:15 +0800

    add by yuxihe

--- examples/backbone/js/app.js
+++ examples/backbone/js/app.js
@@ -4,9 +4,9 @@ var app = app || {};
 var ENTER_KEY = 13;
 var ESC_KEY = 27;
 
-$(function () {
-	'use strict';
+$(function() {
+    'use strict';
 
-	// kick things off by creating the `App`
-	new app.AppView();
+    // kick things off by creating the `App`
+    new app.AppView();
 });
--- examples/backbone/js/models/todo.js
+++ examples/backbone/js/models/todo.js
@@ -1,26 +1,27 @@
 /*global Backbone */
 var app = app || {};
 
-(function () {
-	'use strict';
+(function() {
+    'use strict';
 
-	// Todo Model
-	// ----------
+    // Todo Model
+    // ----------
 
-	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
-	app.Todo = Backbone.Model.extend({
-		// Default attributes for the todo
-		// and ensure that each todo created has `title` and `completed` keys.
-		defaults: {
-			title: '',
-			completed: false
-		},
+    // Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+    app.Todo = Backbone.Model.extend({
+        // Default attributes for the todo
+        // and ensure that each todo created has `title` and `completed` keys.
+        // 设置defaults 之后 可以保证每个都有这两个属性
+        defaults: {
+            title: '',
+            completed: false
+        },
 
-		// Toggle the `completed` state of this todo item.
-		toggle: function () {
-			this.save({
-				completed: !this.get('completed')
-			});
-		}
-	});
+        // Toggle the `completed` state of this todo item.
+        toggle: function() {
+            this.save({ // 保存 completed 的属性   保存就用save
+                completed: !this.get('completed')
+            });
+        }
+    });
 })();
